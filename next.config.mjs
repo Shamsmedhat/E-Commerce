840bff8701ea280a5b118a895e4036662cb9dbf8
Shams <shamsmedhat1@gmail.com>
2024-12-13 18:54:34 +0200

    Trying fix deploy issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   logging: {
     fetches: {
       fullUrl: true,
