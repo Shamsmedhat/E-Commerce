08eaf92875420c1930e7fe7a50450c78908060ba
Shams <shamsmedhat1@gmail.com>
2025-01-19 23:15:39 +0200

    Fix vercel deploy error try to delete the route handler

--- src/app/[locale]/(public)/(homepage)/page.tsx
+++ src/app/[locale]/(public)/(homepage)/page.tsx
@@ -10,10 +10,10 @@ import { getCategoriesAction } from "@/lib/actions/categories-actions";
 
 export default async function Page() {
   const categoriesData = await getCategoriesAction();
-  const res = await fetch("http://localhost:3000/api/products");
+  // const res = await fetch("http://localhost:3000/api/products");
 
-  const data = await res.json();
-  console.log("data", data.data.data.products);
+  // const data = await res.json();
+  // console.log("data", data.data.data.products);
 
   // const data = await res.json();
   // console.log("data", data);
--- src/app/api/products/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  const res = await fetch(
-    "https://e-commerce.thelanerealestate.com/v1/products",
-  );
-
-  const data = await res.json();
-
-  return Response.json({ data });
-}
